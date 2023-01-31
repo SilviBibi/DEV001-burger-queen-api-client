f6663eb09ee85fe750cd5ed7e582cf1d706ecd8c
Silvia Falcon <silvi09_24@hotmail.com>
2023-01-31 13:52:30 -0500

    CRUD productos lista

--- vite-project/src/componentes/Products.jsx
+++ vite-project/src/componentes/Products.jsx
@@ -32,12 +32,42 @@ const Products = () => {
   const createData = (data) => {
     data.id = Date.now();
     // console.log(data)
-    setDb([...db, data])
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" }
+    };
+    api.post(url, options)
+      .then((res) => {
+        console.log(res);
+        if (!res.err) {
+          setDb([...db, res])
+        } else {
+          setError(res);
+        };
+      });
+
   };
 
   const updateData = (data) => {
-    let newData = db.map(el => el.id === data.id ? data : el);
-    setDb(newData);
+    let endpoint = `${url}/${data.id}`
+    // console.log(endpoint)
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" }
+    };
+    api.put(endpoint, options)
+      .then((res) => {
+        console.log(res);
+        if (!res.err) {
+          let newData = db.map(el => el.id === data.id ? data : el);
+          setDb(newData);
+        } else {
+          setError(res);
+        };
+      });
+
+
   };
 
   const deleteData = (id) => {
@@ -45,8 +75,20 @@ const Products = () => {
       `¿Estás seguro de eliminar el producto con el id '${id}'?`
     );
     if (isDelete) {
-      let newData = db.filter(el => el.id !== id);
-      setDb(newData);
+      let endpoint = `${url}/${id}`
+      let options = {
+        headers: { "content-type": "application/json" }
+      };
+      api.del(endpoint, options)
+        .then((res) => {
+          console.log(res);
+          if (!res.err) {
+            let newData = db.filter(el => el.id !== id);
+            setDb(newData);
+          } else {
+            setError(res);
+          };
+        });
     } else {
       return;
     }
@@ -63,11 +105,11 @@ const Products = () => {
       />
       {loading && <Loader />}
       {error && (
-      <Message 
-      msg={`Error ${error.status}: ${error.statusText}`}
-       bgColor="#dc3545" 
-       />
-       )}
+        <Message
+          msg={`Error ${error.status}: ${error.statusText}`}
+          bgColor="#dc3545"
+        />
+      )}
 
       {db && <Table
         data={db}
