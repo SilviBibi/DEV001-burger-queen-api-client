3563ab7512b41bc4e11abe4101ab45037ba6831d
Valeria Murguia <valeria.murguia.g@gmail.com>
2023-02-02 16:03:20 -0600

    Componentes Home, Login y Waiter listos, CSS en proceso

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Home from "./componentes/Home";
 import Login from "./componentes/Login";
 import Products from "./componentes/Products";
 import Waiter from "./componentes/Waiter";
@@ -9,9 +10,10 @@ export function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/products" element={<Products/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
       <Route path="/waiter" element={<Waiter/>}/>
+      <Route path="/products" element={<Products/>}/>
       <Route path="/chef" element={<Chef/>}/>
     </Routes>
     </>
--- vite-project/src/componentes/Waiter.jsx
+++ vite-project/src/componentes/Waiter.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import background from '../../public/Img/background-burger2.png';
+import { Link, useNavigate } from "react-router-dom";
+import './Waiter.css'
 
 const Waiter = () => {
     return (
         <>
-           <p>
-            Hola soy mesero
-           </p>
+           <section className="waiter">
+                <img src={background} alt="background-burger" className="background-burger" />
+                <div className="btn-container">
+                    <Link to="/desayunos" className="btn-menu">DESAYUNOS</Link>
+                    <Link to="/comidas" className="btn-menu">HAMBURGUESA Y ACOMPAÑAMIENTOS</Link>
+                    <Link to="/bebidas" className="btn-menu">BEBIDAS</Link>
+                    <Link to="/products" className="btn-menu">ADMIN</Link>
+                </div>
+           </section>
         </>
     );
 }
