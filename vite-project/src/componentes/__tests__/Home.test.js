7f51da62dd1b9dcef96258e6fc48f1ad1478b36a
Valeria Murguia <valeria.murguia.g@gmail.com>
2023-02-14 16:10:17 -0600

    Config babel-jest-react testing library, carpeta mock y tests corren bien

--- vite-project/src/componentes/__tests__/Home.test.js
+++ vite-project/src/componentes/__tests__/Home.test.js
@@ -1,8 +1,9 @@
 import {render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Home from '../Home'
 
 test('Home component renders login link', () => {
-    render(<Home />);
+    render(<Home />,{wrapper:MemoryRouter});
     const linkElement = screen.getByText(/INICIAR SESIÓN/i);
     expect(linkElement).toBeInTheDocument();
 });
