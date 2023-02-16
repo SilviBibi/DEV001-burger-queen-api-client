2f765bfccbbabc97c80437cb1fd65e8ce0f09f0d
Valeria Murguia <valeria.murguia.g@gmail.com>
2023-02-16 11:53:19 -0600

    Agregar productos en proceso

--- vite-project/src/componentes/Ordenes.jsx
+++ vite-project/src/componentes/Ordenes.jsx
@@ -2,43 +2,41 @@ import React from 'react'
 import deleteIcon from '../../public/Img/delete-icon.png';
 import { useState } from 'react';
 
-const Ordenes = () => {
-        
-//     return (
-//         <>
-//             <div className="ordenes">
-//                 <p>ORDEN</p>
-//                 <input type="text" className="input-class2" placeholder="Nombre del Cliente"></input>
-//             </div>
-//             <div className="containerOrden-sendKitchen">
-//                 <div className="containerOrden">
-//                     <div className="resumenOrden">
-//                         <p className="productSelected">Café americano</p>
-//                         <button className="btnMenos">-</button>
-//                         <p className="counter">1</p>
-//                         <button className="btnMas">+</button>
-//                         <p className="price">$ 5</p>
-//                         <img src={deleteIcon} alt="delete-icon" className="delete-icon" />
-//                     </div>
-//                     <div className="resumenOrden2">
-//                         <p className="productSelected">Jugo de frutas</p>
-//                         <button className="btnMenos">-</button>
-//                         <p className="counter">1</p>
-//                         <button className="btnMas">+</button>
-//                         <p className="price">$ 7</p>
-//                         <img src={deleteIcon} alt="delete-icon" className="delete-icon" />
-//                     </div>
-//                     <div className="total">
-//                         <p className="productSelected">Total</p>
-//                         <p className="productSelected">$ 12 </p>
-//                     </div>
-//                 </div>
-//                 <div className="btnSendKitchen">
-//                     <button className="btn-kitchen">MANDAR A COCINA</button>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
+const Ordenes = (props) => {
+     console.log(props)
+     
+    return (
+        <>
+            <div className="ordenes">
+                <p>ORDEN</p>
+                <input type="text" className="input-class2" placeholder="Nombre del Cliente"></input>
+            </div>
+
+
+            <div className="containerOrden-sendKitchen">
+                <div className="containerOrden">
+                    <div className="resumenOrden2">
+                        <p className="productSelected">Name</p>
+                        <button className="btnMenos">-</button>
+                        <p className="counter">1</p>
+                        <button className="btnMas">+</button>
+                        <p className="price">Price</p>
+                        <img src={deleteIcon} alt="delete-icon" className="delete-icon" />
+                    </div>
+                    <div className="total">
+                        <p className="productSelected">Total</p>
+                        <p className="productSelected">$ </p>
+                    </div>
+                </div>
+
+
+                <div className="btnSendKitchen">
+                    <button className="btn-kitchen">MANDAR A COCINA</button>
+                </div>
+            </div>
+        </>
+    )
 }
+
+
 export default Ordenes
--- vite-project/src/componentes/Prueba.jsx
+++ vite-project/src/componentes/Prueba.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import Prueba2 from './Prueba2'
 import './Cards.css'
+import Ordenes from './Ordenes'
 
 const Prueba = ({ data }) => {
-      return (
-    <>
-        {data.length > 0 ? (
+    return (
+        <>
+            {data.length > 0 ? (
                 data.map(product => (
-                    <Prueba2
-                        key={product.id}
-                        product={product}
-                        url= {product.url}
-                        name={product.name}
-                        price={product.price}
-                    />
+                        <Prueba2
+                            key={product.id}
+                            product={product}
+                            url={product.url}
+                            name={product.name}
+                            price={product.price}
+                        />
                 ))
             ) : (
-                
-                    <p>Sin datos</p>
-                
+
+                <p>Sin datos</p>
+
             )}
-    </>
-      )}
+        </>
+    )
+}
 
 export default Prueba
