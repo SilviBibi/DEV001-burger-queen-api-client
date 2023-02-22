abc9deb53610997c9f74d0fd4a370724058c4b2d
Valeria Murguia <valeria.murguia.g@gmail.com>
2023-02-21 18:07:43 -0600

    Menú hamburguesa listo, total y borrar en proceso

--- vite-project/src/componentes/Desayunos.jsx
+++ vite-project/src/componentes/Desayunos.jsx
@@ -4,18 +4,43 @@ import background from '../../public/Img/background-coffee2.png';
 import { Link, useNavigate } from "react-router-dom";
 import './Desayunos.css'
 import logo from '../../public/Img/logo-white.png';
-import menuIcon from '../../public/Img/menu-icon.png';
 import Ordenes from "./Ordenes";
-import { useState } from "react"; 
+import { useState } from "react";
 
 const Desayunos = (props) => {
 
     return (
         <>
+            <div>
+                <nav className="navbar navbar-dark fixed-top">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#"><img src={logo} alt="bq-logo" className="bq-logo2" /></a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                            <div className="offcanvas-header">
+                                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div className="offcanvas-body">
+                                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                    <li className="nav-item">
+                                        <Link to="/desayunos" className="nav-link active" aria-current="page" href="#">MENÚ</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/pedidos" className="nav-link" href="#">PEDIDOS</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/" className="nav-link" href="#">CERRAR SESIÓN</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
             <section className="desayunos" data-testid="desayunos-1">
                 <img src={background} alt="background-coffe" className="background-coffe" />
-                <img src={logo} alt="bq-logo" className="bq-logo2" />
-                <img src={menuIcon} alt="menu-icon" className="menu-icon-desayunos" />
                 <div className="menu-desayunos">
                     <div className="btns-container">
                         <Link to="/desayunos" className="btn-desayunos">Desayunos</Link>
@@ -27,7 +52,7 @@ const Desayunos = (props) => {
                     </div>
                 </div>
                 <div className="containerOrdenes">
-                    <Ordenes/>
+                    <Ordenes />
                 </div>
             </section>
         </>
